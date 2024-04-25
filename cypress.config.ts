8f4711e5513fa45bf19cbe8eb9aaa139dc336479
jrexiti <jay.rexiti@gmail.com>
2024-04-25 18:17:22 -0500

    same test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "cypress";
-import * as createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   e2e: {
     specPattern: "**/*.feature",
-    video: false,
     async setupNodeEvents(
       on: Cypress.PluginEvents,
       config: Cypress.PluginConfigOptions
---
+++ cypress/e2e/Form.feature
@@ -0,0 +1,7 @@
+Feature: Form submission
+
+Scenario: Valid form submission
+Given user navigates to google page
+When user enters a search name
+When user clicks on search
+
