81a362895581738e68c4b1d035589c43a0f2f8a5
jrexiti <jay.rexiti@gmail.com>
2024-07-14 14:05:02 -0500

    some tests

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,5 +1,6 @@
 ///  <reference types="Cypress" />
 
+
 describe("template spec", () => {
   it("this is the test case desciption ", () => {
     cy.visit("http://localhost:5173/");
@@ -9,8 +10,8 @@ describe("template spec", () => {
 
   it("should display the page title", () => {
     cy.visit("http://localhost:5173/");
-    cy.get('h1').should('exist');
-    cy.get('h1').should('have.length', 1);
+    cy.get("h1").should("exist");
+    cy.get("h1").should("have.length", 1);
     cy.get("h1").contains("My Cypress Course Tasks");
   });
 });
---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,11 @@
+///  <reference types="Cypress" />
+
+describe("task management", () => {
+  it("open and close the new task modal", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+  });
+});
