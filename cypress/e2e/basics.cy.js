a212b8c0fc83e94c736b113864bd83a0000ddc34
jrexiti <jay.rexiti@gmail.com>
2024-07-14 13:32:19 -0500

    test test

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,5 +1,16 @@
+///  <reference types="Cypress" />
+
 describe("template spec", () => {
   it("this is the test case desciption ", () => {
     cy.visit("http://localhost:5173/");
+
+    cy.get(".main-header img");
+  });
+
+  it("should display the page title", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get('h1').should('exist');
+    cy.get('h1').should('have.length', 1);
+    cy.get("h1").contains("My Cypress Course Tasks");
   });
 });
